47194f274ffed6df2eb4f12b1a167937482362fa
Your Name <you@example.com>
2020-09-15 01:51:13 +0300

    finishing CC7

--- Data-Structures/linkedList/README.md
+++ Data-Structures/linkedList/README.md
@@ -9,6 +9,8 @@ Define a method called toString (or str in Python) which takes in no arguments a
 Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
 Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
 
+Write a method for the Linked List class which takes a number, k, as a parameter. Return the node’s value that is k from the end of the linked list. You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
 ## Approach and Efficiency
 I applied the information I gathered through my reading assignments and class demonstration to build ths application.
 
--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -1,78 +1,172 @@
-'use strict'
+
 class Node {
-  
-    constructor(value, next) {
-      this.value = value;
-      this.next = next;
-    }
-    
+
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
   }
-  
-  class LinkedList {
-  
-    constructor() {
-      this.head = null;
+
+}
+
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {//insert a new head node
+
+    this.head = new Node(value, this.head);
+
+  }
+
+  append(value) {
+
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
     }
-  
-    insert(value) {//insert a new head node
-  
-      this.head = new Node(value, this.head);
-  
+    // Create a while loop to find the end of the list
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
     }
-  
-    append(value) {//insert a new node at the end of the list
-  
-      const newNode = new Node(value);
-  
-      if(!this.head){//beginning a new list (no existing head)
-        this.head = new Node(value);
-        return;
-      }
-      // Create a while loop to find the end of the list
-      let currentNode = this.head;
-      while(currentNode.next){//checks for truthiness of currentNode.next - null will fail the test
-        currentNode = currentNode.next;
-      }
-      currentNode.next = newNode;
+    currentNode.next = newNode;
+  }
+
+  insertBefore(value, newVal) {
+    const newNode = new Node(newVal);
+
+    if (!this.head) {
+      console.log(`List is currently empty - beginning list with new Node ${newVal}`);
+      this.head = newNode;
+      return;
     }
-  
-    includes(value) {
-      if(!this.head){
-        return false;
-      }
-  
-      let currentNode = this.head;
-      while(currentNode.next){
-        if (currentNode.value === value){
-          return true;
+
+    if (!this.includes(value)) {
+      console.log(`${value} is not included in the list - unable to insert new node of ${newVal} before ${value}.`);
+      return;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode.value === value) { //checks FIRST node (edge case)
+      this.head = new Node(newVal, currentNode);
+      return
+    } else {
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          newNode.next = currentNode.next;
+          currentNode.next = newNode;
+          return;
         } else {
           currentNode = currentNode.next;
         }
       }
-      if (currentNode.value === value){
-        return true;
-      } else {
-        return false;
-      }
     }
-  
-    toString() {
-  
-      if(!this.head){
-        return 'NULL';
-      }
-      let currentNode = this.head;
-      
-  
-      let stringOfValues = '';
-      while(currentNode.next){
-        stringOfValues = `${stringOfValues}{ ${currentNode.value} } -> `;
+
+  }
+
+  insertAfter(value, newVal) {
+    const newNode = new Node(newVal);
+
+    if (!this.head) {
+      console.log(`List is currently empty - beginning list with new Node ${newVal}`);
+      this.head = newNode;
+      return;
+    }
+
+    if (!this.includes(value)) {
+      console.log(`${value} is not included in the list - unable to insert new node of ${newVal} after ${value}.`);
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (currentNode.value === value) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+        return;
+      } else {
         currentNode = currentNode.next;
       }
-      return `${stringOfValues}{ ${currentNode.value} } -> NULL`;
     }
+    if (currentNode.value === value) { //checks LAST node (edge case)
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+      return
+    }
+  }
+
+  includes(value) {
+    if (!this.head) {
+      return false;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.value === value) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    if (currentNode.value === value) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  toString() {
+
+    if (!this.head) {
+      return 'NULL';
+    }
+    let currentNode = this.head;
+
+
+    let stringOfValues = '';
+    while (currentNode.next) {
+      stringOfValues = `${stringOfValues}{ ${currentNode.value} } -> `;
+      currentNode = currentNode.next;
+    }
+    return `${stringOfValues}{ ${currentNode.value} } -> NULL`;
+  }
+
+  kthFromEnd(k) {
+    if(!this.head){
+      return 'Exception: list contains no nodes';
+    }
+    if(k<0){
+      return 'Exception: please provide a k value of zero or greater';
+    }
+    let listLength = 0;
+    let currentNode = this.head;
   
+    while(currentNode.next){
+      listLength++;
+      currentNode=currentNode.next;
+    }
+    let targetNode = (listLength - k);
+    if (targetNode<0){
+      return 'Exception: List does not contain sufficient nodes for request';
+    } else {
+      currentNode = this.head;
+      let kCounter = 0
+      while(kCounter < targetNode){
+        kCounter++;
+        currentNode = currentNode.next;
+      }
+    }
+  return currentNode.value;
   }
   
-  module.exports = LinkedList;
-  
+}
+
+
+
+module.exports = LinkedList;
